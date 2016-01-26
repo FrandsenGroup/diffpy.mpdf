8c0681fa41c9eb3ab0a2a193b1bbb30eb6c3d066
Benjamin Frandsen <benfrandsen@gmail.com>
2016-01-26 13:09:05 -0500

    finished example of helical structure

--- doc/examples/example_helical.py
+++ doc/examples/example_helical.py
@@ -14,26 +14,35 @@ from mcalculator import *
 structureFile = "MnO_cubic.cif"
 
 # Create the structure from our cif file
-#
 mnostructure = loadStructure(structureFile)
+# modify the structure to simulate a 1-D material
 mnostructure.lattice.a=3.0
 mnostructure.lattice.b=150.0
 mnostructure.lattice.c=150.0
 
-# Create a magnetic structure object
-helixSpec=magSpecies(mnostructure)
+# Create a magnetic species object
+helix=magSpecies(mnostructure)
 
+# Set up the basis vectors for a helical spin configuration
 Sk=0.5*(np.array([0,0,1])+0.5j*np.array([0,1,0]))
-helixSpec.basisvecs=np.array([Sk,Sk.conj()])
-helixSpec.kvecs=np.array([[np.sqrt(2)/100,0,0],[-1.0*np.sqrt(2)/100,0,0]])
-helixSpec.makeAtoms()
-helixSpec.makeSpins()
+helix.basisvecs=np.array([Sk,Sk.conj()])
 
-x,y,z=np.transpose(helixSpec.atoms)
-sx,sy,sz=np.transpose(helixSpec.spins)
+# Set up the propagation vector
+helix.kvecs=np.array([[np.sqrt(2)/10,0,0],[-1.0*np.sqrt(2)/10,0,0]])
 
-fig=plt.figure()
-ax=fig.add_subplot(111)
-ax.plot(x,sz,'r.',x,sy,'b.',x,sx,'k.')
-plt.show()
+# Populate with atoms and spins
+helix.rmaxAtoms=70.0
+helix.makeAtoms()
+helix.makeSpins()
+helix.label='helix'
 
+# Create the magnetic structure object
+mstruc=magStructure()
+mstruc.loadSpecies(helix)
+mstruc.makeAll()
+
+# Create the mPDF calculator
+mc=mPDFcalculator(mstruc)
+mc.rmax=70.0
+
+mc.plot()
