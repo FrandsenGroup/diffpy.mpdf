corrected the calculation of the linear term for a system with a net moment in the case of S and L being different from 1/2 and 0, respectively. Also updated the method to calculate the net magnetization correctly.

diffpy/mpdf/magstructure.py
@@ -967,14 +967,15 @@ class MagStructure:
 
         """
         if method == 'directCalculation':
-            totalMag = np.sum(self.spins, axis=1)
-            self.netMag = totalMag / self.spins.shape[0]
+            totalMag = np.sum(self.spins, axis=0) * np.mean(self.gfactors)
+            self.netMag = np.linalg.norm(totalMag) / self.spins.shape[0]
         if method == 'speciesCalculation':
             mags = []
             weights = []
             if self.struc != []:
                 for key in self.species:
-                    mag = np.linalg.norm(self.species[key].spins[0]) # assumes constant magnitude of spins
+                    g = self.species[key].gS + self.species[key].gL
+                    mag = g * np.linalg.norm(self.species[key].spins[0]) # assumes constant magnitude of spins
                     mags.append(mag)
                     weight = np.sum(self.struc.occupancy[self.species[key].strucIdxs])
                     weights.append(weight)

diffpy/mpdf/magutils.py
@@ -1162,11 +1162,11 @@ def calculatemPDF(xyz, sxyz, gfactors=np.array([2.0]), calcIdxs=np.array([0]),
             linearTerm = opt.x[0] * r * np.exp(-r/corrLength)
     else:
         if corrLength == 0:
-            linearTerm = 4 * np.pi * r * rho0 * (2.0/3.0) * netMag**2 \
-                         * K1 / (1.913 * 2.81794 / 2.0) ** 2
+            linearTerm = 4 * np.pi * r * rho0 * (2.0/3.0) * netMag**2  \
+                         / ( K1 / (1.913 * 2.81794 / 2.0) ** 2)
         else:
             linearTerm = 4 * np.pi * r * rho0 * (2.0 / 3.0) * netMag ** 2 * np.exp(-r/corrLength) \
-                         * K1 / (1.913 * 2.81794 / 2.0) ** 2
+                          / ( K1 / (1.913 * 2.81794 / 2.0) ** 2)
     fr -= linearTerm
 
 